b2ff5cec82144b514b5931e6a5f043ab733f15a1
nguyen-fran <nguyenfrancisco87@gmail.com>
2020-10-03 13:41:09 -0700

    Started gamestate class

--- app/src/main/java/edu/up/strategogamestate/StrategoGameState.java
+++ app/src/main/java/edu/up/strategogamestate/StrategoGameState.java
@@ -1,4 +1,19 @@
 package edu.up.strategogamestate;
 
 public class StrategoGameState {
+    int[] playerGY = new int[11];
+    int[] oppGY = new int[11];
+    boolean playerturn; //true if player's turn, false if com's turn
+    //TODO: this
+    // BoardSquares[][] boardSquares;
+
+    public StrategoGameState() {
+        for (int i = 0; i < playerGY.length; i++) {
+            playerGY[i] = 0;
+            oppGY[i] = 0;
+        }
+
+
+    }
+
 }
