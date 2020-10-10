fa403ab94b7ed644d2a03d145a5ca53ab7cd34da
g-marshak <marshak22@up.edu>
2020-10-10 14:37:20 -0700

    lots of header comment formatting, filling out attack/movement methods more, adding helper methods

--- app/src/main/java/edu/up/strategogamestate/StrategoGameState.java
+++ app/src/main/java/edu/up/strategogamestate/StrategoGameState.java
@@ -53,7 +53,7 @@ public class StrategoGameState {
     /**
      * deep copy
      *
-     * @param orig
+     * @param orig original game state being copied
      */
     public StrategoGameState(StrategoGameState orig) {
         this.gamePhase = orig.gamePhase;
@@ -73,43 +73,50 @@ public class StrategoGameState {
 
     /**
      *method for moving the piece on a given board square to another square
-     * TODO: see if it's possible to make this nicer looking, reduce amount of if/else statements
+     * TODO: make sure turn indexing/player vs computer turns make sense
+     * TODO: need to also somehow check that piece on square you are selecting is you own (can't move opponent's pieces)
      * @param square
      * @param newX
      * @param newY
+     * @param playerIndex who is trying to make the move
      * @return
      */
-    public boolean move(BoardSquare square, int newX, int newY){
+    public boolean move(BoardSquare square, int newX, int newY, boolean playerIndex){
         GamePiece piece = square.getPiece();
 
-        //check if it's the player's turn (if not, then move is illegal)
-        if(canMove( HUMANTURN /*current player, don't have player classes in yet so defaulting to human*/) == false){
+        //check if it's the player's turn (if not, then move is illegal), or if new square is out of range
+        if(!canMove(playerIndex) || !squareOnBoard(newX, newY)){
             return false;
         }
 
-        //check if coordinates you want to move to are in range and valid for piece (special exception for scout range, and immobile pieces)
-        if(piece.getRank() == 2){ //special movement range for scout
-            //TODO: special movement logic here (may need to make helper method)
+        //check if coordinates you want to move to are valid for piece (special exception for scout range, and immobile pieces)
+        if(piece.getRank() == 2){ //checking validity for scout (special range)
+            if(scoutMove(square, newX, newY) == false){
+                return false;
+            }
         } else if(piece.getRank() == 11 || piece.getRank() == 0){ //immobile pieces (cannot move)
             return false;
         }else{ //all other pieces have normal movement range (check if square is in range)
             if(newX > square.getxPos() + 1 || newX < square.getxPos() - 1 ||
-               newY > square.getyPos() + 1 || newY < square.getyPos() - 1){
+               newY > square.getyPos() + 1 || newY < square.getyPos() - 1 ||){
                 return false;
             }
         }
 
 
-        //if new coords are occupied by another piece (not null), then attack
-        if(boardSquares[newX][newY].getOccupied() == true && boardSquares[newX][newY].getPiece() == null){
+        //if new coords are occupied by another piece (not null or player's), then attack
+        if(boardSquares[newX][newY].getOccupied() == true &&
+           (boardSquares[newX][newY].getPiece() == null ||
+           boardSquares[newX][newY].getPiece().getTeam() != playerIndex)){
+            //moving into occupied square that you cannot attack is an illegal move
             return false;
         }else if(boardSquares[newX][newY].getOccupied() == true){
-            attack(square.getPiece(), boardSquares[newX][newY].getPiece());
+            attack(square.getPiece(), boardSquares[newX][newY].getPiece(), playerIndex);
         }
 
-        //if current board piece is now captured, then remove piece from square (potential graveyard update happens in attack method)
-        //if not, then update new board square
-        if(square.getPiece().getCaptured() == true){
+        //check initial square/new square pieces for capture
+        //update init/new square pieces appropriately
+        if(square.getPiece().getCaptured()){
             square.setPiece(null);
         }else{
             boardSquares[newX][newY].setPiece(square.getPiece());
@@ -119,22 +126,49 @@ public class StrategoGameState {
         return true;
     }
 
-    /** TODO: fill this method out
+    /**
      * method for logic of attacks between pieces
      * determines which piece (or both) get captured and sent to the graveyard
-     * @param piece1
-     * @param piece2
-     * @return
+     * assuming attack is valid move, and that there are both an attacking and defending piece
+     * @param piece1 piece initiating the attack
+     * @param piece2 piece being attacked
+     * @param playerIndex who is making the attack (assuming it is their turn to move)
+     * @return true if attack was successful, false if something in the comparison went wrong
      */
-    public boolean attack(GamePiece piece1, GamePiece piece2){
-        //first check for special cases (bomb, spy, miner)
+    public boolean attack(GamePiece piece1, GamePiece piece2, boolean playerIndex){
+        //first check for special cases (spy, miner)
+        if(piece1.getRank() == 1 && piece2.getRank() == 10){ //spy attacking marshal
+            piece2.setCaptured(true);
+        }else if(piece1.getRank() == 5 && piece2.getRank() == 11){ //miner attacking bomb
+            piece2.setCaptured(true);
+        }else if(piece1.getRank() < piece2.getRank()){ //attacker gets captured
+            piece1.setCaptured(true);
+        }else if(piece1.getRank() > piece2.getRank()){ //defender gets captured
+            piece2.setCaptured(true);
+        }else if(piece1.getRank() == piece2.getRank()){ //both get captured
+            piece1.setCaptured(true);
+            piece2.setCaptured(true);
+        }else{ //some other combination, indicates something went wrong
+            return false;
+        }
 
-        //otherwise compare numerical rank
+        //updating graveyard(s)
+        if(playerIndex == HUMANTURN){ //human attacking computer
+            if(piece1.getCaptured() == true){ //updating player graveyard
+                setPlayerGYIdx(piece1.getRank() - 1, getPlayerGY()[piece1.getRank() - 1] + 1);
+            }
+            if(piece2.getCaptured() == true){ //updating opponent graveyard
+                setOppGYIdx(piece2.getRank() - 1, getOppGY()[piece2.getRank() - 1] + 1);
+            }
+        }else if(playerIndex == COMPTURN){ //computer attacking human
+            if(piece1.getCaptured() == true){ //updating opponent graveyard
+                setOppGYIdx(piece1.getRank() - 1, getOppGY()[piece1.getRank() - 1] + 1);
+            }
+            if(piece2.getCaptured() == true){ //updating player graveyard
+                setPlayerGYIdx(piece2.getRank() - 1, getPlayerGY()[piece2.getRank() - 1] + 1);
+            }
 
-        //depending on ranking, either one or both pieces will have their captured value change
-
-        //update graveyard(s)
-
+        }
         return true;
     }
 
@@ -151,7 +185,38 @@ public class StrategoGameState {
         }
     }
 
+    /**
+     * helper method to determine if selected square is a valid move for a scout piece
+     * assumes that it is able to move at all (turn is correct)
+     * assumes that new square is in range of board
+     * @param square initial board square that the scout piece is on
+     * @param newX x coord that you are trying to move the scout to
+     * @param newY y coord that you are trying to move the scout to
+     * @return true if movement is valid, false if not
+     */
+    public boolean scoutMove(BoardSquare square, int newX, int newY){
+        //check if given square shares only one of x or y value (trying to check if in straight line)
+
+        //check if there are any occupied spaces in between selected square and current square
+    }
+
+    /**
+     * helper method to determine if given square is on the board or not
+     * @param x x coord of square being checked
+     * @param y y coord of square being checked
+     * @return true if square is within range of the board, false if not
+     */
+    public boolean squareOnBoard(int x, int y){
+        if(x > BOARD_SIZE || y > BOARD_SIZE || x < 0 || y < 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     //getters and setters
+
+    //getters
     public boolean getGamePhase(){
         return gamePhase;
     }
@@ -168,6 +233,7 @@ public class StrategoGameState {
         return boardSquares;
     }
 
+    //setters
     public void setGamePhase(boolean gamePhase){
         this.gamePhase = gamePhase;
     }
@@ -177,8 +243,18 @@ public class StrategoGameState {
     public void setPlayerGY(int[] playerGY) {
         this.playerGY = playerGY;
     }
+    public void setPlayerGYIdx(int index, int val){
+        if(index >= 0 && index < playerGY.length ){
+            this.playerGY[index] = val;
+        }
+    }
     public void setOppGY(int[] oppGY) {
         this.oppGY = oppGY;
+    }
+    public void setOppGYIdx(int index, int val){
+        if(index >= 0 && index < oppGY.length ){
+            this.oppGY[index] = val;
+        }
     }
     public void setBoardSquares(BoardSquare[][] boardSquares) {
         this.boardSquares = boardSquares;
